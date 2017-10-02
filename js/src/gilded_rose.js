005071e021af7b60f1a44e5a2a04d7b2cec725a2
Fanny Cheung <fanny@ynote.hk>
2017-10-03 01:25:28 +0200

    Only use functions in rules

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,11 +1,4 @@
-class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
-
+// Each rule should be exclusive, so we pick the first matching one.
 const itemRules = (item, rules) => {
   return rules.filter(rule => rule.item(item))[0].rules
 }
@@ -18,6 +11,7 @@ const itemRule = (item, rules) => {
   })[0]
 }
 
+// Items rules
 const isBasic = item => {
   return !isConjured(item) &&
     !isBackstagePasses(item) &&
@@ -41,6 +35,7 @@ const isAgedBrie = item => {
   return item.name.match(/^Aged Brie/)
 }
 
+// Items rules predicates
 const isExpired = item => {
   return item.sellIn <= 0
 }
@@ -57,25 +52,29 @@ const backstagePassesPredicates = {
   },
 }
 
-const update = (item, options) => {
+// Updates methods
+const canUpdateQuality = currentQuality => (
+  currentQuality > 0 && currentQuality < 50
+)
+
+const updateQuality = quality => item => {
+  if (!canUpdateQuality(item.quality)) return item
+
+  return (item.quality += quality, item)
+}
+
+const updateSellIn = sellIn => item => (item.sellIn += sellIn, item)
+
+// Update strategy
+const _pipe = (f, g) => (...args) => g(f(...args))
+const pipe = (...fns) => fns.reduce(_pipe)
+
+const update = options => item => {
   const attributes = Object.keys(options)
+  const updates = attributes.map(attribute => options[attribute])
+  const updateAttributes = pipe.apply(this, updates)
 
-  attributes.forEach(attribute => {
-    item[attribute] += attributeDelta(attribute, options[attribute], item)
-  })
-
-  return item
-}
-
-const isQuality = attribute => attribute == 'quality'
-const canUpdateQuality = currentQuality => {
-  return currentQuality > 0 && currentQuality < 50
-}
-
-const attributeDelta = (attribute, value, item) => {
-  if (isQuality(attribute) && !canUpdateQuality(item.quality)) return 0
-
-  return value
+  return updateAttributes(item)
 }
 
 const rules = [
@@ -84,16 +83,16 @@ const rules = [
     rules: [
       {
         predicate: item => !isExpired(item),
-        deltas: {
-          quality: -1,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(-1),
+          sellIn: updateSellIn(-1),
         },
       },
       {
         predicate: isExpired,
-        deltas: {
-          quality: -2,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(-2),
+          sellIn: updateSellIn(-1),
         }
       },
     ],
@@ -103,16 +102,16 @@ const rules = [
     rules: [
       {
         predicate: item => !isExpired(item),
-        deltas: {
-          quality: 1,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(1),
+          sellIn: updateSellIn(-1),
         },
       },
       {
         predicate: isExpired,
-        deltas: {
-          quality: 2,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(2),
+          sellIn: updateSellIn(-1),
         },
       },
     ]
@@ -121,9 +120,9 @@ const rules = [
     item: isLegendary,
     rules: [
       {
-        deltas: {
-          quality: 0,
-          sellIn: 0,
+        updates: {
+          quality: updateQuality(0),
+          sellIn: updateQuality(0),
         },
       }
     ]
@@ -133,16 +132,16 @@ const rules = [
     rules: [
       {
         predicate: item => !isExpired(item),
-        deltas: {
-          quality: -2,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(-2),
+          sellIn: updateSellIn(-1),
         },
       },
       {
         predicate: isExpired,
-        deltas: {
-          quality: -4,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(-4),
+          sellIn: updateSellIn(-1),
         }
       },
     ]
@@ -152,57 +151,56 @@ const rules = [
     rules: [
       {
         predicate: isExpired,
-        deltas: {
-          quality: item => item.quality * -1,
-          sellIn: -1,
+        updates: {
+          quality: item => (item.quality = 0, item),
+          sellIn: updateSellIn(-1),
         },
       },
       {
         predicate: backstagePassesPredicates.isLimited,
-        deltas: {
-          quality: 3,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(3),
+          sellIn: updateSellIn(-1),
         }
       },
       {
         predicate: backstagePassesPredicates.isFresh,
-        deltas: {
-          quality: 2,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(2),
+          sellIn: updateSellIn(-1),
         }
       },
       {
         predicate: backstagePassesPredicates.isEarlyBird,
-        deltas: {
-          quality: 1,
-          sellIn: -1,
+        updates: {
+          quality: updateQuality(1),
+          sellIn: updateSellIn(-1),
         }
       },
     ]
   },
 ]
 
+const updateShopItems = items => {
+  return items.map(item => {
+    const currentItemRules = itemRules(item, rules)
+    const currentRule = itemRule(item, currentItemRules)
+
+    return update(currentRule.updates)(item)
+  })
+}
+
+class Item {
+  constructor(name, sellIn, quality){
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  updateQuality() {
-    return this.items.map(item => {
-      const currentItemRules = itemRules(item, rules)
-      const currentRule = itemRule(item, currentItemRules)
-
-      let quality = currentRule.deltas.quality
-
-      if (typeof quality == 'function') {
-        quality = currentRule.deltas.quality(item)
-      }
-
-      const options = {
-        quality: quality,
-        sellIn: currentRule.deltas.sellIn,
-      }
-
-      return update(item, options)
-    })
-  }
+  updateQuality() { return updateShopItems(this.items) }
 }
